176ec35f262c55c8d62a85ef635f4ae23ad9ca09
Dhvani Shah <dhvanihshah06@gmail.com>
2025-12-09 11:21:34 +0530

    Lab 6

--- npm-lab-1/Lab6.js
+++ npm-lab-1/Lab6.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 // fs.readFile('data.txt',(err,data)=>{
 //     if(err){
 //         console.log("Error Occured : ",err)
-        
 //     }
 //     else if (data){
 //             console.log("Data : ",data.toString());
