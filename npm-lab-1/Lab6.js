3081886fe2a7121b6a7525de3d0120433a378ebd
Dhvani Shah <dhvanihshah06@gmail.com>
2025-12-12 10:15:43 +0530

    Lab 6

--- npm-lab-1/Lab6.js
+++ npm-lab-1/Lab6.js
@@ -1,27 +1,36 @@
-//read file readfilesync write append unlink makefoldermydirandcheckexist
 const fs = require('fs');
 
-// fs.readFile('data.txt',(err,data)=>{
+// 1 read file
+// fs.readFile('data.txt',"utf-8",(err,data)=>{
 //     if(err){
 //         console.log("Error Occured : ",err)
 //     }
-//     else if (data){
-//             console.log("Data : ",data.toString());
+//     else{
+//             // console.log("Data : ",data.toString());
+//             console.log("Data : ",data)
 //         }
 // })
 
-// const ans = fs.readFileSync('data.txt')
-// console.log("Sync Data : ",ans.toString());
+// 2 read Sync
+// try{
+//     const ans = fs.readFileSync('data.txt',"utf-8");
+//     console.log("Sync : ");
+//     console.log(ans);
+// }catch(err){
+//     console.log(err);
+// }
 
+// 3 write file
 // fs.writeFile('Output.txt',"This is Write File Demo",(err)=>{
 //     if(err){
 //         console.log("Error : ",err);
 //     }
 //     else{
-//         console.log("File NWrite Successfully!!!");
+//         console.log("File Write Successfully!!!");
 //     }
 // })
 
+// 4 append file
 // fs.appendFile('Output.txt',"\nThis is Appended Text",(err)=>{
 //     if(err){
 //         console.log("Error : ",err)
@@ -31,9 +40,23 @@ const fs = require('fs');
 //     }
 // })
 
+//5 fs unlink
+fs.unlink('data.txt',(err)=>{
+    if(err){
+        console.log("Error : ",err);
+    }
+    else{
+        console.log("Deleted Successfully ");
+    }
+})
+
+
+
+
 // fs.exists('data.txt',(exists)=>{
 //     console.log("File Exists : ",exists);
 // });
+
 // fs.stat('./data.txt',(err,data)=>{
 //    if(err){
 //         console.log("Error Occured : ",err)
