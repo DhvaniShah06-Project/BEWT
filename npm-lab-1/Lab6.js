17ae4719441ec3a823e0e4563352ada85bb04cf6
Dhvani Shah <dhvanihshah06@gmail.com>
2025-12-12 11:19:41 +0530

    Lab 6

--- npm-lab-1/Lab6.js
+++ npm-lab-1/Lab6.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
-// 1 read file
+ //1 read file
 // fs.readFile('data.txt',"utf-8",(err,data)=>{
 //     if(err){
 //         console.log("Error Occured : ",err)
 //     }
 //     else{
-//             // console.log("Data : ",data.toString());
+//             // console.log("Data : ",data.toString()); instead of utf-8 can do this
 //             console.log("Data : ",data)
 //         }
 // })
@@ -20,7 +20,7 @@ const fs = require('fs');
 //     console.log(err);
 // }
 
-// 3 write file
+ //3 write file
 // fs.writeFile('Output.txt',"This is Write File Demo",(err)=>{
 //     if(err){
 //         console.log("Error : ",err);
@@ -40,15 +40,65 @@ const fs = require('fs');
 //     }
 // })
 
-//5 fs unlink
-fs.unlink('data.txt',(err)=>{
-    if(err){
-        console.log("Error : ",err);
-    }
-    else{
-        console.log("Deleted Successfully ");
-    }
-})
+// 5 fs unlink
+// fs.unlink('data.txt',(err)=>{
+//     if(err){
+//         console.log("Error : ",err);
+//     }
+//     else{
+//         console.log("Deleted Successfully ");
+//     }
+// })
+
+// 6 folder my-data using  make dir 
+// fs.mkdir('my-data',(err)=>{
+//     if(err){
+//         if(err.code==="EEXIST"){
+//             console.log("Folder Already Exist!!!");
+//         }else{
+//             console.log("Error : ",err);
+//         }
+//         return;
+//     }
+//         console.log("Folder Created Successfully!!")
+// })
+
+// 7  list all file in folder called documents fs.readdir  print file names one by one
+// fs.readdir("my-data",(err,files)=>{
+//     if(err){
+//         console.log("Error : ",err);
+//         return;
+//     }
+//     console.log("Files in documents : ",files)
+//     files.forEach(file=>{
+//         console.log(file);
+//     })
+// })
+
+ //8 copies file name source.txt to new file backup.txt fs.copyfile
+//  fs.copyFile("Output.txt","Backup.txt",(err)=>{
+//     if(err){
+//         console.log("Error : ",err)
+//         return;
+//     }
+//     else{
+//         console.log("File Copied");
+//     }
+//  })
+
+// 9 check if config.json exists in current directory fsExistSync
+// if(fs.existsSync("config.json")){
+//     console.log("Exists");
+// }
+// else{
+//     console.log("File Not Exists");
+// }
+
+//10 fs.watch() monitor change in watchme.txt whenever file content change print file change
+// fs.watch("Output.txt",()=>{
+//     console.log("File Change");
+// })
+// console.log("Watching Output.txt File... (Edit Output.txt to see result!!)")
 
 
 
